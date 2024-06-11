d7b86f5597eee146358b4e8d696e98c28def752d
GuzelLik <162622171+Guzel2022Lik@users.noreply.github.com>
2024-06-11 17:04:37 +0300

    Update 3 Numbers.py

--- Numbers.py
+++ Numbers.py
@@ -14,7 +14,6 @@ print(revert_int_option_1(12345))
 print(revert_int_option_2(12345))
 
 
-
 # 1.2 Напишите программу, которая проверяет, является ли число числом Армстронга. 
 # Число Армстронга — натуральное число, которое в данной системе счисления равно сумме своих цифр, возведённых в степень, равную количеству его цифр.
 
@@ -26,3 +25,15 @@ def armstrong_number(num):
     return sum == num
 
 print(armstrong_number(371))
+
+
+# 1.3 Напишите программу, которая проверит каждое число в списке на предмет того, является ли оно простым.
+
+def prime_num(num):
+    for div_num in range(2, num):
+        if num % div_num == 0:
+            return False
+    return True
+
+for item in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
+    print(f"{item}: {prime_num(item)}")
