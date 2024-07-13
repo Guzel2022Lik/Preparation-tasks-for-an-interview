7ab19356a889c169ed8b6a108bf4dac11a705cd9
GuzelLik <162622171+Guzel2022Lik@users.noreply.github.com>
2024-07-13 13:21:05 +0300

    Update 2 Numbers_2.py

--- Numbers_2.py
+++ Numbers_2.py
@@ -17,3 +17,26 @@ def factorize(num):
 print(factorize(5))
 print(factorize(52))
 print(factorize(63))
+
+
+# Напишите программу, которая проверит, что число является совершенным. 
+# Совершенное число - натуральное число, равное сумме всех своих собственных делителей. 
+# Например, число 6 равно сумме своих собственных делителей 1 + 2 + 3. 
+# Примеры совершенных чисел: 6, 28, 496, 8128
+
+
+def get_divisors(num):
+    divisors = []
+    for i in range(1, num):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def perfect_num(num):
+    divisors = get_divisors(num)
+    return num == sum(divisors)
+
+print(perfect_num(6))
+print(perfect_num(28))
+print(perfect_num(496))
+print(perfect_num(8128))
