eece128fec5e3e2c4a9b9d1a7f7215e47e92fc8c
GuzelLik <162622171+Guzel2022Lik@users.noreply.github.com>
2024-10-26 18:56:49 +0300

    Update Numbers_3.py

--- Numbers_3.py
+++ Numbers_3.py
@@ -9,3 +9,14 @@ def remove_sym(string: str, symbol: str):
 
 print(remove_sym('This is my text', ' '))
 print(remove_sym('This is my text', 's'))
+
+
+# Задача 2.2. Условие
+
+# Напишите программу, которая подсчитает количество вхождений подстроки в строку
+
+def count_str(string: str, substring: str):
+    return string.count(substring)
+
+print(count_str('This is my text', ' '))
+print(count_str('This is my text', 'is'))
